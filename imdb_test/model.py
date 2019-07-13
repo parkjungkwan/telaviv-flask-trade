6f3c3e381d6d5d28a46e9812838706d5ae5fca96
pakjkwan <pakjkwan@gmail.com>
2019-07-13 17:40:48 +0900

    17:40

--- imdb_test/model.py
+++ imdb_test/model.py
@@ -1,6 +1,5 @@
-import tensorflow_datasets as tfds
 import tensorflow as tf
-import tensorflow_hub as hub
+from tensorflow import keras
 import numpy as np
 
 class Imdb:
@@ -8,25 +7,9 @@ class Imdb:
         pass
 
     def create_model(self):
-        train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
-
-        (train_data, validation_data), test_data = tfds.load(
-            name="imdb_reviews",
-            split=(train_validation_split, tfds.Split.TEST),
-            as_supervised=True)
-        # 데이터 탐색
-
-        train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
-        # 10개의 샘플
-        # print('10개의 샘플 %s'% (train_examples_batch))
-        # print('10개의 라벨 %s' % (train_labels_batch))
-        embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
-        hub_layer = hub.KerasLayer(embedding, input_shape=[],
-                                   dtype=tf.string, trainable=True)
-        hub_layer(train_examples_batch[:3])
-        model = tf.keras.Sequential()
-        model.add(hub_layer)
-        model.add(tf.keras.layers.Dense(16, activation='relu'))
-        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-        print(model.summary())
+        imdb = keras.datasets.imdb
+        (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+        print("훈련 샘플: {}, 레이블: {}".format(len(train_data), len(train_labels)))
+        print("검증 샘플: {}, 레이블: {}".format(len(test_data), len(test_data)))
+        # 첫번째 리뷰
+        print(train_data[0])
---
+++ keras_test/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras_test.model import KerasModel
+
+if __name__ == '__main__':
+    k = KerasModel()
+    k.create_model()
